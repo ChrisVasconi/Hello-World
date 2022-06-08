3f3493ff7b6e02506c3844d9c2d0fc73d9c18774
Chris Vasconi <vasconichris@gmail.com>
2022-06-08 19:21:27 -0400

    hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+print("Hello World!")
+
+name = "Chris"
+print(name, "!")
+print(name, "hello world!")
+
+name = 42
+print("Hello,", name, "!")
+print("Hello", + name)
+
+
+fave_food1 = "sushi"
+fave_food2 = "Pizza"
+print("I love to eat", fave_food1, "and", fave_food2)
